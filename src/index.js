5ec67cde8ba4ec11a671cb0ea7bf51e6d8f711bd
Hiral <hiral@Hirals-MacBook-Pro.local>
2020-04-07 01:17:45 +0530

    dashboard done

---
+++ src/index.js
@@ -0,0 +1,87 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+});
+function Cars() {
+        this.cities = [];
+        this.filterdData = [];
+        this.searchText = '';
+        this.isHd = '';
+        this.isOneway = '';
+        this.renderCard = (data) => {
+            let parent;
+            if (data.popular) parent = document.getElementById('popular');
+            else parent = document.getElementById('others');
+            let row = document.createElement('div');
+            row.setAttribute('class', 'card');
+            let img = document.createElement('img');
+            img.setAttribute('src', data.icon);
+            img.setAttribute('alt', 'No Image found');
+            let title = document.createElement('div');
+            title.setAttribute('class', 'title');
+            title.textContent = data.name;
+            row.appendChild(img);
+            row.appendChild(title);
+            parent.appendChild(row);
+        }
+        this.fetchData = () => {
+        fetch('https://api.zoomcar.com/v4/cities?platform=web').then(function (response) {
+            // The API call was successful!
+            return response.json();
+        }).then((data) => {
+            if (data.cities) {
+                this.cities = data.cities
+                for (let i = 0; i < this.cities.length; i++) {
+                    this.renderCard(this.cities[i])
+                }
+            }
+            else {
+                console.warn('No Data Found', err);
+            }
+    
+        }).catch(function (err) {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        });
+    }
+}
+Cars.prototype.filterCities = function(event) {
+    console.log(this);
+    if (event.type == 'checkbox') {
+        if (event.checked) {
+            this.isHd = event.id == 'hd' ? true : this.isHd;
+            this.isOneway = event.id == 'oneWay' ? true : this.isOneway;
+        }
+        else {
+            // handling unselect part here
+            this.isHd = event.id == 'hd' ? '' : this.isHd;
+            this.isOneway = event.id == 'oneWay' ? '' : this.isOneway;
+        }
+    }
+    else {
+        this.searchText = event;
+    }
+    this.filterdData = this.cities.filter((d) =>
+        d.name.toLowerCase().includes(this.searchText) &&
+        (this.isHd ? d.hd_enabled == this.isHd : [true, false].includes(d.hd_enabled)) &&
+        (this.isOneway ? d.one_way_enabled == this.isOneway : [true, false].includes(d.one_way_enabled))
+    );
+    document.getElementById('popular').textContent = '';
+    document.getElementById('others').textContent = '';
+    for (let i = 0; i < this.filterdData.length; i++) {
+       this.renderCard(this.filterdData[i]);
+    }
+    console.log(this.filterdData);
+}
+var op = new Cars();
+function debounce(fn, duration) {
+    let timer;
+    return function (args) {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            fn(args);
+        }, duration)
+    }
+}
+const fn = debounce(op.filterCities.bind(op), 200);
+op.fetchData();
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    entry: [
+      './src/index.js',
+      './src/style.scss'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "script-loader",
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1,
+                localIdentName: "[name]_[local]_[hash:base64]",
+                sourceMap: true,
+                minimize: true
+              }
+            }
+          ]
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              'style-loader',
+              // Translates CSS into CommonJS
+              'css-loader',
+              // Compiles Sass to CSS
+              'sass-loader',
+            ],
+          }
+      ]
+    }
+  };
